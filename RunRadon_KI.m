4283aecc429d4a1ad489205392f263ef0f3a5aff
Kirill Ivanov <kirillpskov@gmail.com>
2023-04-25 04:20:32 -0700

    added plotting of 18_10-11

--- RunRadon_KI.m
+++ RunRadon_KI.m
@@ -24,98 +24,17 @@ clear all
 close all
 
 %KI RunRadon for Europa with data processed with ImpDAR. 
-window = 201;
+window = 101;
 angle_thresh = [6 6];
 plotter = 0;
 movie_flag = 0;
 load(impdar_convert('18_11_bot_cut.mat'))
-%load('proc_cresis.mat')
 %surface_bottom=[surface_elev(end)+1e-3-surface_elev;surface_elev(end)+1e-3-bed_elev];
 Data = imgaussfilt(real(20*log(Data)));
-[r c] = size(Data);
-Data = imresize(Data,[round(r/1) round(c/2)],"nearest");
+%[r c] = size(Data);
+%Data = imresize(Data,[round(r/1) round(c/2)],"nearest");
 Data(isinf(Data)) = NaN;
-[slopegrid_x,slopegrid_y,slopegrid,opt_x,opt_y,opt_angle]=RollingRadon_KI(data_x(1:2:end),Time,Data,window,angle_thresh, ...
-    plotter,surface_bottom(1:2:end),movie_flag);
+[slopegrid_x,slopegrid_y,slopegrid,opt_x,opt_y,opt_angle]=RollingRadon_KI(data_x,Time,Data,window,angle_thresh, ...
+    plotter,surface_bottom,movie_flag);
 
-
-
-%a=find(new_data(:,end)>0); %bed cutoff
-% first cut off bottom of data (Below bed)
-%NewData=new_data(a(1):8000,:);
-%data_y=surface_elev(end)-depth_axis(a(1):8000); % this needs to be depth, not elevation. 
-
-%addpath('/Users/crosson/Documents/MATLAB/Radargramming/CSARP_standard');
-% data_x_or_filename = 'Data_20160512_02_009.mat';
-% window = 301;
-% angle_thresh = [6 6]; % maybe in degrees? subtracts 5 at one point??
-% plotter = 1;
-% movie_flag = 0;
-% %others
-%  load('Data_20160512_02_009.mat');
-%  load('BottomQuick0512_02_009.mat');
-%[new_data shift_amount new_time] = elevation_shift(Data,Time,Elevation,Surface);
-%[new_data shift_amount depth_axis surface_elev bed_elev] = depth_shift(Data,Time,Surface,Elevation,botinter,1);
-% [new_data shift_amount depth_axis surface_elev bed_elev] = depth_shift(fliplr(Data),Time,fliplr(Surface),fliplr(Elevation),fliplr(BOTTOM),1);
-% a=find(new_data(:,end)>0);
-
-
-
-
-% to get data_x need to convert Lat and Lon into polar
-%[x,y]=polarstereo_fwd(Latitude,Longitude,2);
-%data_x=distance_vector(x,y,0);
-
-
-
-% save 301_66_radon_51602_006.mat
-
-% some plotting functions
-                                                                                
-% %simple:
-% figure; imagesc(slopegrid_x,slopegrid_y,slopegrid)
-% xlabel('Distance (meters)')
-% ylabel('Depth below highest point (meters)')
-% colorbar
-% colormap(parula) %you can change this as well
-% caxis([-3 3]) % or whatever
-% ?
-%%
-% figure;
-[x,y]=meshgrid(slopegrid_x,slopegrid_y);
-figure; surf(x,y,-(slopegrid),'EdgeColor','interp');
-view(180,90)
-colorbar
-colormap(redblue)
-caxis([-9 9])
-hold on
-plot(data_x, surface_elev(end)-bed_006+350,'k','LineWidth',3)
-% % 
-% %% for the radar itself - beware these run kind of slowly
-% figure;
-%[x,y]=meshgrid(data_x,data_y);
-[x,y]=meshgrid(data_x,data_y);
-figure; surface(x,y,log10((NewData.^2)),'EdgeColor','interp');
-view(180,90)
-colormap(bone)
-colorbar
-caxis([-72 -52]) 
-hold on
-plot(data_x, surface_elev(end)-bed_006+350,'b','LineWidth',3)
-
-% % % ?
-% figure;
-% [x,y]=meshgrid(data_x,data_y);
-% [x,y]=meshgrid(data_x,data_y);
-% figure; f=pcolor(x(1:2:end,1:2:end),y(1:2:end,1:2:end),log10((NewData(1:2:end,1:2:end).^2)))
-% shading interp;
-% set(gca,'YDir','reverse')
-% colormap(bone)
-% colorbar
-% caxis([5.4 5.75]) 
-
-% %% slices through example
-% figure; plot((slopegrid(18:end,620:630)),'r')
-% hold on; plot(mean(slopegrid(18:end,620:630)'),'r','LineWidth',5)
-% plot((slopegrid(18:end,655:665)),'b')
-% hold on; plot(mean(slopegrid(18:end,655:665)'),'b','LineWidth',5)
+save('18_11_full.mat')
--- slope_plot_KI.m
+++ slope_plot_KI.m
@@ -1,6 +1,9 @@
 %% Init
+clear all
+close all
+
 radar = 0; % plot radargram as a subplot with the slope plot
-line = 0; % 0 - 19_11, 1 - 18_10-11, 2 - 18_6-7-8
+line = 1; % 0 - 19_11, 1 - 18_10-11, 2 - 18_6-7-8
 
 %% Load
 load('CustomColormap.mat')
@@ -10,7 +13,11 @@ if line == 0
     bed = bed(:,4)';
 elseif line == 1
     A = load("18_10_full.mat");
+    bed_A = readmatrix("18_10_bot.csv");
+    bed_A = bed_A(:,4)';
     B = load("18_11_full.mat");
+    bed_B = readmatrix("18_11_bot.csv");
+    bed_B = bed_B(:,4)';
 else
     A = load('19_11_hill_slope.mat');
     B = load("19_11_lake_slope.mat");
@@ -22,16 +29,16 @@ end
 %% Graphing
 if line == 0
     %Slope
-    test1 = -slopegrid;
-    test1(test1 == 5) = NaN;
+    slp = -slopegrid;
+    slp(slp == 5) = NaN;
     yd = max(Elevation) - Time/2 * 1.69e8;
     xd = data_x*1000;
     %
-    sourceSize = size(test1);
+    sourceSize = size(slp);
     Data_size = size(Data);
     targetSize = [Data_size(1)-window+1,Data_size(2)];
     [X_samples,Y_samples] = meshgrid(linspace(1,sourceSize(2),targetSize(2)), linspace(1,sourceSize(1),targetSize(1)));
-    source_resized_to_target_size = interp2(test1, X_samples, Y_samples);
+    source_resized_to_target_size = interp2(slp, X_samples, Y_samples);
     full_target = [nan((window-1)/2,size(source_resized_to_target_size,2)); source_resized_to_target_size];
     full_target = vertcat(full_target,nan((window-1)/2,size(source_resized_to_target_size,2)));
     figure(1);
@@ -64,5 +71,93 @@ if line == 0
         xlabel(ax2,'Distance, km')
         ylabel(ax2,'Elevation from sea level, m')
         linkaxes([ax1, ax2], 'x','y');
+    end
+end
+
+if line == 1 || line == 2
+    %Slope
+    if line == 1
+        xd = zeros(1);
+        yd = [];
+        for i = 1:2
+            if i == 1
+                S = A;
+            else 
+                S = B;
+            end
+            %slope
+            slp = -S.slopegrid;
+            slp(slp == 6) = NaN;
+            yd = [yd; max(S.Elevation) - (S.Time/2 * 1.69e8)];
+            xd = [xd (S.data_x*1000 + max(xd))];
+            %resize
+            sourceSize = size(slp);
+            Data_size = size(S.Data);
+            targetSize = [Data_size(1)-S.window+1,Data_size(2)];
+            [X_samples,Y_samples] = meshgrid(linspace(1,sourceSize(2),targetSize(2)), linspace(1,sourceSize(1),targetSize(1)));
+            source_resized_to_target_size = interp2(slp, X_samples, Y_samples);
+            full_target = [nan((S.window-1)/2,size(source_resized_to_target_size,2)); source_resized_to_target_size];
+            full_target = vertcat(full_target,nan((S.window-1)/2,size(source_resized_to_target_size,2)));
+            if i ==1
+                figure(1);
+                sgtitle('18 _ 10+11, Wndw - 100, Flow -->>')
+            end
+            if radar == 1
+                ax1 = subplot(211);
+            else
+                ax1 = axes;
+            end
+            if i == 1
+                %surf(ax1,xd,yd,full_target,'EdgeColor','none','FaceColor','interp')
+                %hold on
+                %plot(ax1,xd, (S.Elevation - bed_A(end:-1:1)),'k',"LineWidth",3)
+                xlabel(ax1,'Distance, km')
+                ylabel(ax1,'Elevation from sea level, m')
+                Z1 = full_target;
+                Elevation = S.Elevation;
+                bed_fix_A = Elevation - bed_A(end:-1:1);
+            end
+            if i == 2
+                cut = find(~isnan(full_target(:,1)));
+                cut = cut(1);
+                Z1 = [nan(cut,size(Z1,2)); Z1];
+                Z2 = vertcat(full_target,nan(cut,size(full_target,2)));
+                Elevation = [S.Elevation Elevation];
+                bed_fix_B = S.Elevation - bed_B(end:-1:1);
+                %surf(ax1,xd+shift_amount,yd,full_target,'EdgeColor','none','FaceColor','interp')
+                %view(180,90)
+                %imagesc(ax1,xd,yd,full_target)
+            end
+            
+            %plot(ax1,xd,S.Elevation,'k','LineWidth',3)
+            %hold on
+            % Radargram
+            if radar == 1
+                ax2 = subplot(212);
+                imagesc(ax2,xd, yd(end:-1:1), Data)
+                colormap(ax2,'bone')
+                colorbar
+                set(ax2, 'XDir', 'reverse');
+                xlabel(ax2,'Distance, km')
+                ylabel(ax2,'Elevation from sea level, m')
+                linkaxes([ax1, ax2], 'x','y');
+            end
+        end
+        Data_size = size(Z1);
+        ydd = linspace(max(yd),min(yd),Data_size(1));
+        surf(xd(2:end),ydd,[Z2 Z1],'FaceColor','interp','EdgeColor','none')
+        view(180,90)
+        shading interp
+        clim([-1,1])
+        colormap(ax1,flipud(CustomColormap))
+        cbar = colorbar(ax1);
+        set(ax1, 'YDir', 'reverse');
+        cbar.Label.String = 'Reflector Slope, Degrees';
+        hold on
+        plot(xd(2:end),Elevation,'k','LineWidth',3)
+        plot(xd(2:end), [bed_fix_B bed_fix_A] ,'k',"LineWidth",3)
+    else
+        for i = [A B C]
+        end
     end
 end
