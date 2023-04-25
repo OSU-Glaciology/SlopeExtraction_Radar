ecc873da4383a42d91f690f24d7ebe0eecb423d4
Kirill Ivanov <kirillpskov@gmail.com>
2023-04-25 04:35:44 -0700

    added radar plot for line 18_10-11

--- slope_plot_KI.m
+++ slope_plot_KI.m
@@ -2,7 +2,11 @@
 clear all
 close all
 
-radar = 0; % plot radargram as a subplot with the slope plot
+
+% plot radargram as a subplot with the slope plot
+radar = 0; % 0 - no, 1 - yes 
+
+%choose line
 line = 1; % 0 - 19_11, 1 - 18_10-11, 2 - 18_6-7-8
 
 %% Load
@@ -100,7 +104,7 @@ if line == 1 || line == 2
             full_target = vertcat(full_target,nan((S.window-1)/2,size(source_resized_to_target_size,2)));
             if i ==1
                 figure(1);
-                sgtitle('18 _ 10+11, Wndw - 100, Flow -->>')
+                sgtitle('18 _ 10+11, Wndw - 100, Flow <<--')
             end
             if radar == 1
                 ax1 = subplot(211);
@@ -108,39 +112,21 @@ if line == 1 || line == 2
                 ax1 = axes;
             end
             if i == 1
-                %surf(ax1,xd,yd,full_target,'EdgeColor','none','FaceColor','interp')
-                %hold on
-                %plot(ax1,xd, (S.Elevation - bed_A(end:-1:1)),'k',"LineWidth",3)
                 xlabel(ax1,'Distance, km')
                 ylabel(ax1,'Elevation from sea level, m')
                 Z1 = full_target;
                 Elevation = S.Elevation;
+                Data = S.Data;
                 bed_fix_A = Elevation - bed_A(end:-1:1);
             end
             if i == 2
                 cut = find(~isnan(full_target(:,1)));
                 cut = cut(1);
                 Z1 = [nan(cut,size(Z1,2)); Z1];
+                Data = [nan(cut,size(Data,2)); Data];
                 Z2 = vertcat(full_target,nan(cut,size(full_target,2)));
-                Elevation = [S.Elevation Elevation];
+                Data_fix = vertcat(S.Data,nan(cut,size(S.Data,2)));
                 bed_fix_B = S.Elevation - bed_B(end:-1:1);
-                %surf(ax1,xd+shift_amount,yd,full_target,'EdgeColor','none','FaceColor','interp')
-                %view(180,90)
-                %imagesc(ax1,xd,yd,full_target)
-            end
-            
-            %plot(ax1,xd,S.Elevation,'k','LineWidth',3)
-            %hold on
-            % Radargram
-            if radar == 1
-                ax2 = subplot(212);
-                imagesc(ax2,xd, yd(end:-1:1), Data)
-                colormap(ax2,'bone')
-                colorbar
-                set(ax2, 'XDir', 'reverse');
-                xlabel(ax2,'Distance, km')
-                ylabel(ax2,'Elevation from sea level, m')
-                linkaxes([ax1, ax2], 'x','y');
             end
         end
         Data_size = size(Z1);
@@ -154,8 +140,19 @@ if line == 1 || line == 2
         set(ax1, 'YDir', 'reverse');
         cbar.Label.String = 'Reflector Slope, Degrees';
         hold on
-        plot(xd(2:end),Elevation,'k','LineWidth',3)
+        plot(xd(2:end),[S.Elevation Elevation],'k','LineWidth',3)
         plot(xd(2:end), [bed_fix_B bed_fix_A] ,'k',"LineWidth",3)
+        % Radargram
+        if radar == 1
+            ax2 = subplot(212);
+            imagesc(ax2,xd(2:end), ydd(end:-1:1), [Data_fix Data])
+            colormap(ax2,'bone')
+            colorbar
+            set(ax2, 'XDir', 'reverse');
+            xlabel(ax2,'Distance, km')
+            ylabel(ax2,'Elevation from sea level, m')
+            linkaxes([ax1, ax2], 'x','y');
+        end
     else
         for i = [A B C]
         end
